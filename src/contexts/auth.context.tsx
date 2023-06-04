59032e24e50ba7e53271f3c6a5d67601ce8d7694
ngocdiemnguyen99 <nn.diem1710@gmail.com>
2023-06-04 17:14:48 +0700

    Fix bug when token is  expirated

--- src/contexts/auth.context.tsx
+++ src/contexts/auth.context.tsx
@@ -7,21 +7,28 @@ interface AppContextInterface {
   setIsAuthenticated: Dispatch<React.SetStateAction<boolean>>
   profile: User | null
   setProfile: Dispatch<React.SetStateAction<User | null>>
+  reset: () => void
 }
 
 const initialAuth: AppContextInterface = {
   isAuthenticated: Boolean(getAccessTokenLS()),
   setIsAuthenticated: () => null,
   profile: getProfileFromLS(),
-  setProfile: () => null
+  setProfile: () => null,
+  reset: () => null
 }
 const AuthContext = createContext<AppContextInterface>(initialAuth)
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(initialAuth.isAuthenticated)
   const [profile, setProfile] = useState<User | null>(initialAuth.profile)
+
+  const reset = () => {
+    setIsAuthenticated(false)
+    setProfile(null)
+  }
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, profile, setProfile }}>
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, profile, setProfile, reset }}>
       {children}
     </AuthContext.Provider>
   )
