fa5646a41e4c6a255bdbd13f4c696248f57c7e36
ngocdiemnguyen99 <nn.diem1710@gmail.com>
2023-06-05 23:08:34 +0700

    [UI] User SideNav

--- src/components/IconSvg/IconSvg.tsx
+++ src/components/IconSvg/IconSvg.tsx
@@ -385,3 +385,22 @@ export const CartButton = ({ className }: IconProps) => {
     </svg>
   )
 }
+
+export const EditIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      className={className}
+      width={12}
+      height={12}
+      viewBox='0 0 12 12'
+      xmlns='http://www.w3.org/2000/svg'
+      style={{ marginRight: 4 }}
+    >
+      <path
+        d='M8.54 0L6.987 1.56l3.46 3.48L12 3.48M0 8.52l.073 3.428L3.46 12l6.21-6.18-3.46-3.48'
+        fill='#9B9B9B'
+        fillRule='evenodd'
+      />
+    </svg>
+  )
+}
